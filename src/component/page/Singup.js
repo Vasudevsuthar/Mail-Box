991b4e9294bc20d93cbbb350e084645d4b8f5647
Vasudev <Vasudev@codewithvasudev.com>
2023-12-13 12:04:44 +0530

    Task 01

---
+++ src/component/page/Singup.js
@@ -0,0 +1,111 @@
+import React, { useRef, useState } from "react";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import Card from "react-bootstrap/Card";
+import classes from "./Singup.module.css";
+
+const Singup = () => {
+  const emailInputRef = useRef();
+  const passwordInputRef = useRef();
+  const confirmPasswordInputRef = useRef();
+
+  const [isLoading, setIsLoading] = useState(false);
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const enteredEmail = emailInputRef.current.value;
+    const enteredPassword = passwordInputRef.current.value;
+    const enteredConfirmPassword = confirmPasswordInputRef.current.value;
+
+    if (enteredPassword !== enteredConfirmPassword) {
+      alert("Passwords do not match");
+      return;
+    }
+
+    setIsLoading(true);
+
+    fetch(
+      "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBAYFEF5InzJ-FOrwWVY6_IJ3zMV_ne8Oc",
+      {
+        method: "POST",
+        body: JSON.stringify({
+          email: enteredEmail,
+          password: enteredPassword,
+          returnSecureToken: true,
+        }),
+        headers: {
+          "content-type": "application/json",
+        },
+      }
+    )
+      .then((res) => {
+        setIsLoading(false);
+        if (res.ok) {
+          return res.json();
+        } else {
+          return res.json().then((data) => {
+            let errorMessage = "Authentication failed";
+
+            throw new Error(errorMessage);
+          });
+        }
+      })
+      .then((data) => {
+        console.log("Account created successfully");
+        console.log(data);
+        alert("Account created successfully");
+
+        emailInputRef.current.value = "";
+        passwordInputRef.current.value = "";
+        confirmPasswordInputRef.current.value = "";
+      })
+      .catch((err) => {
+        alert(err.message);
+      });
+  };
+
+  return (
+    <div className={classes.Container}>
+      <Card className={classes.card}>
+        <Card.Title className={classes.title}>SingUp</Card.Title>
+        <Form onSubmit={submitHandler}>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Email</Form.Label>
+            <Form.Control
+              ref={emailInputRef}
+              type="email"
+              placeholder="Enter your email"
+            />
+            <Form.Text className="text-muted"></Form.Text>
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              ref={passwordInputRef}
+              type="password"
+              placeholder="Password"
+            />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="ConfirmPassword">
+            <Form.Label>Confirm Password</Form.Label>
+            <Form.Control
+              ref={confirmPasswordInputRef}
+              type="password"
+              placeholder="Confirm Password"
+            />
+          </Form.Group>
+          <Button className={classes.Button} variant="primary" type="submit">
+            SingUp
+          </Button>
+        </Form>
+        <Button className={classes.Button} variant="secondary" type="submit">
+          Have an account? Login here
+        </Button>
+      </Card>
+    </div>
+  );
+};
+
+export default Singup;
